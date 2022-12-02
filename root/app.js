23fd082f4449b12c67afa0f5e757b1744bc059b4
harman-d97 <77417939+harman-d97@users.noreply.github.com>
2022-12-02 15:24:25 -0500

    added code for deployment

--- root/app.js
+++ root/app.js
@@ -3,10 +3,18 @@ var indexRouter = require('./routes/index');
 var cors = require('cors');
 
 const app = express();
+const PORT = process.env.PORT || 4000;
+const path = require('path');
 app.use(cors());
 app.use(express.json());
 app.use('/', indexRouter);
 
-app.listen(4000, () => {
-    console.log('Listening on port 4000');
+app.use(express.static(__dirname + '/frontend/dist/easy-maintenance-manager'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/dist/easy-maintenance-manager/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Listening on port ${PORT}');
 });
