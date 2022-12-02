b36231795d550b5062f0d7240a00cf62b4ad9dc2
harman-d97 <77417939+harman-d97@users.noreply.github.com>
2022-12-02 17:39:41 -0500

    msg

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/', indexRouter);
 
-app.use(express.static('./frontend/dist/easy-maintenance-manager'));
+app.use(express.static(__dirname + '/frontend/dist/easy-maintenance-manager'));
 
-app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: 'frontend/dist/easy-maintenance-manager' });
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/frontend/dist/easy-maintenance-manager/index.html'));
 });
  
 app.listen(PORT, () => {
