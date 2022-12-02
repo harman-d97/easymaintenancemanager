dd2e8dea8e30506a18f858f425c3632cc965f319
harman-d97 <77417939+harman-d97@users.noreply.github.com>
2022-12-02 17:08:30 -0500

    update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: 'us-cdbr-east-06.cleardb.net/',
     user: 'b6b826afdc15cd',
     password: '7848ad71',
@@ -21,19 +21,21 @@ router.post('/register', async function (req, res, next) {
         const hashedPassword = md5(password.toString());
 
         const checkUsername = `SELECT username FROM users WHERE username = ?`;
-        connection.query(checkUsername, [username],  (err, result, fields) => {
-            if (typeof result == 'undefined' || result.length == 0) {
-                const sql = `INSERT INTO users (employeeId , adminAccess, username, password, firstName, lastName, businessCode) VALUES (?, ?, ?, ?, ?, ?, ?)`;
-                connection.query(sql, [employeeId, 0, username, hashedPassword, firstName, lastName, sixDigitCode], (err, result, fields) => {
-                    if (err) {
-                        console.log('error occured while inserting user');
-                        res.send({status: 0, data: err});
-                    } else {
-                        let token = jwt.sign({data: result}, 'secret');
-                        res.send({status: 1, data: result, token: token});
-                    }
-                });
-            }
+        pool.getConnection(function(err, connection) {
+            connection.query(checkUsername, [username],  (err, result, fields) => {
+                if (typeof result == 'undefined' || result.length == 0) {
+                    const sql = `INSERT INTO users (employeeId , adminAccess, username, password, firstName, lastName, businessCode) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+                    connection.query(sql, [employeeId, 0, username, hashedPassword, firstName, lastName, sixDigitCode], (err, result, fields) => {
+                        if (err) {
+                            console.log('error occured while inserting user');
+                            res.send({status: 0, data: err});
+                        } else {
+                            let token = jwt.sign({data: result}, 'secret');
+                            res.send({status: 1, data: result, token: token});
+                        }
+                    });
+                }
+            });
         });
     } catch (error) {
         console.log('error on first query');
@@ -60,13 +62,15 @@ router.post('/login', async function (req, res) {
         const hashedPassword = md5(password.toString());
 
         const sql = `SELECT * FROM users WHERE username = ? AND password = ?`;
-        connection.query(sql, [username, hashedPassword], function(err, result, fields) {
-            if (err || result.length == 0) {
-                res.send({status: 0, data: err});
-            } else {
-                let token = jwt.sign({ data: result }, 'secret');
-                res.send({ status: 1, data: result, token: token });
-            }
+        pool.getConnection(function(err, connection) {
+            connection.query(sql, [username, hashedPassword], function(err, result, fields) {
+                if (err || result.length == 0) {
+                    res.send({status: 0, data: err});
+                } else {
+                    let token = jwt.sign({ data: result }, 'secret');
+                    res.send({ status: 1, data: result, token: token });
+                }
+            });
         });
     } catch (error) {
         res.send({status: 0, data: error});
@@ -76,13 +80,15 @@ router.post('/login', async function (req, res) {
 router.get('/get-all-employees', async function (req, res) {
     try {
         const sql = `SELECT employeeId, firstname, lastname FROM users WHERE adminAccess = 0`;
-        connection.query(sql, (err, result, fields) => {
-            if (err) {
-                console.log('Error occured while getting all employees');
-                res.send({status: 0, data: err});
-            } else {
-                res.send({status: 1, data: result});
-            }
+        pool.getConnection(function(err, connection) {
+            connection.query(sql, (err, result, fields) => {
+                if (err) {
+                    console.log('Error occured while getting all employees');
+                    res.send({status: 0, data: err});
+                } else {
+                    res.send({status: 1, data: result});
+                }
+            });
         });
     } catch (error) {
         console.log('Error occured during get all employees query');
@@ -93,13 +99,15 @@ router.get('/get-all-employees', async function (req, res) {
 router.get('/get-all-users', async function (req, res) {
     try {
         const sql = `SELECT employeeId, firstname, lastname FROM users`;
-        connection.query(sql, (err, result, fields) => {
-            if (err) {
-                console.log('Error occured while getting all users');
-                res.send({status: 0, data: err});
-            } else {
-                res.send({status: 1, data: result});
-            }
+        pool.getConnection(function(err, connection) {
+            connection.query(sql, (err, result, fields) => {
+                if (err) {
+                    console.log('Error occured while getting all users');
+                    res.send({status: 0, data: err});
+                } else {
+                    res.send({status: 1, data: result});
+                }
+            });
         });
     } catch (error) {
         console.log('Error occured during get all users query');
@@ -111,13 +119,15 @@ router.post('/delete-user', async function (req, res) {
     try {
         let { employeeId } = req.body;
         const sql = `DELETE FROM users WHERE employeeId = ?`;
-        connection.query(sql, [employeeId], (err, result, fields) => {
-            if (err) {
-                console.log('Error occured while deleting user');
-                res.send({status: 0, data: err});
-            } else {
-                res.send({status: 1, data: result});
-            }
+        pool.getConnection(function(err, connection) {
+            connection.query(sql, [employeeId], (err, result, fields) => {
+                if (err) {
+                    console.log('Error occured while deleting user');
+                    res.send({status: 0, data: err});
+                } else {
+                    res.send({status: 1, data: result});
+                }
+            });
         });
     } catch (error) {
         console.log('Error occured during delete user query');
@@ -129,13 +139,15 @@ router.post('/give-admin-access', async function (req, res) {
     try {
         let { employeeId } = req.body;
         const sql = `UPDATE users SET adminAccess = 1 WHERE employeeId = ?`;
-        connection.query(sql, [employeeId], (err, result, fields) => {
-            if (err) {
-                console.log('Error occured while promoting user to admin');
-                res.send({status: 0, data: err});
-            } else {
-                res.send({status: 1, data: result});
-            }
+        pool.getConnection(function(err, connection) {
+            connection.query(sql, [employeeId], (err, result, fields) => {
+                if (err) {
+                    console.log('Error occured while promoting user to admin');
+                    res.send({status: 0, data: err});
+                } else {
+                    res.send({status: 1, data: result});
+                }
+            });
         });
     } catch (error) {
         console.log('Error occured during admin access query');
